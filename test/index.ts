e385b4534205c4b0206b74de3d485c4136ecbdd0
Hongfei Shen <hongfei.shen@dolphindb.com>
2022-09-17 11:49:18 +0800

    test support colors;

--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,12 @@
+import util from 'util'
+
 import { DDB } from '../index.js'
 import { test_print } from './print.js'
 import { test_types } from './types.js'
 
 ;(async function test () {
+    util.inspect.defaultOptions.colors = true
+    
     console.log('--- 测试开始 ---')
     
     console.log('连接到 ws://127.0.0.1:8848')

d10a5864ef7dbeab0673bbfe82462ea510423852
Hongfei Shen <hongfei.shen@dolphindb.com>
2022-12-13 15:48:45 +0800

    deps;

--- test/streaming.ts
+++ test/streaming.ts
@@ -4,6 +4,26 @@ import { DDB } from '../index.js'
 export async function test_streaming (ddb: DDB) {
     console.log('测试流数据')
     
+    await ddb.eval(
+        'try {\n' +
+        "    if (!defined('prices', SHARED)) {\n" +
+        '        share(\n' +
+        '            streamTable(\n' +
+        '                10000:0,\n' +
+        "                ['time', 'stock', 'price'],\n" +
+        '                [TIMESTAMP, SYMBOL, DOUBLE]\n' +
+        '            ),\n' +
+        "            'prices'\n" +
+        '        )\n' +
+        "        print('prices 流表创建成功')\n" +
+        '    } else\n' +
+        "        print('prices 流表已存在')\n" +
+        '} catch (error) {\n' +
+        "    print('prices 流表创建失败')\n" +
+        '    print(error)\n' +
+        '}\n'
+    )
+    
     let sddb = new DDB('ws://127.0.0.1:8848', {
         streaming: {
             table: 'prices',
@@ -14,6 +34,20 @@ export async function test_streaming (ddb: DDB) {
     })
     
     await sddb.connect()
+    
+    await ddb.eval(
+        'n = 5\n' +
+        '\n' +
+        'for (i in 0..9)\n' +
+        '    append!(\n' +
+        '        prices,\n' +
+        '        table([\n' +
+        '            (now() + 0..(n-1)) as time,\n' +
+        "            take(['MSFT', 'FUTU'], n) as stock,\n" +
+        '            (0..(n-1) \\ 10) as price\n' +
+        '        ])\n' +
+        '    )\n'
+    )
     
     sddb.disconnect()
 }

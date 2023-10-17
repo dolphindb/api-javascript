cf0eadf9a7cdf93ce40e6d87bfb0fc6fd93d9e86
Sicheng Du <sicheng.du@dolphindb.com>
2023-10-17 15:45:51 +0800

    流数据列过滤优化

--- browser.ts
+++ browser.ts
@@ -4120,16 +4120,24 @@ export class DDB {
         }
         
         let schema: DdbTableObj
-        const column_filter = this.streaming.filters.column
-        const { value: colnames } = await this.call<DdbVectorStringObj>('publishTable', [
-                'localhost',
-                new DdbInt(0),
-                this.streaming.table,
-                (this.streaming.action ||= `api_js_${new Date().getTime()}`),
-                new DdbVoid(),  // offset
-                column_filter ? column_filter : new DdbVoid()// filter
-                // allow exists
-            ], { 
+        
+        const column_filter = this.streaming?.filters?.column
+        const params = [
+            'localhost',
+            new DdbInt(0),
+            this.streaming.table,
+            (this.streaming.action ||= `api_js_${new Date().getTime()}`)
+        ]
+        const { value: colnames } = await this.call<DdbVectorStringObj>('publishTable',
+            column_filter
+                ? [
+                    ...params,
+                    new DdbVoid(),  // offset
+                    column_filter ? column_filter : new DdbVoid()// filter
+                    // allow exists
+                ]
+                : params 
+            , { 
                 skip_connection_check: true, 
                 
                 // 先准备好收到 websocket message 的 callback

458df967b7b9e9c4bbf4a1ec2cb7e0e1dbf6fad7
Chengchen Zheng <chengchen.zheng@dolphindb.com>
2024-01-15 13:28:33 +0800

    refactor

--- shared/utils.ts
+++ shared/utils.ts
@@ -1,4 +1,4 @@
-import { nulls, DdbType } from '../constants.js'
+import { nulls, DdbType } from './constants.js'
 
 
 export function is_decimal_type (type: DdbType) {
@@ -13,3 +13,12 @@ export function is_decimal_null_value (type: DdbType, value: number | bigint) {
         (value === nulls.int32 && type === DdbType.decimal32)
     )
 }
+
+export function parse_duration_code (code: number) {
+    let str = String.fromCharCode((code >> 24) & 255)
+    str += String.fromCharCode((code >> 16) & 255)
+    str += String.fromCharCode((code >> 8) & 255)
+    str += String.fromCharCode(code & 255)
+    return str
+}
+
